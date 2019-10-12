6beacc682a80351796a269b13af7adcc4dd3c5f2
Mike <mchlrmn@me.com>
2019-10-12 17:51:35 +0200

    Add Github login, frontend, gravatar

--- src/authentication.ts
+++ src/authentication.ts
@@ -1,22 +1,38 @@
-import { ServiceAddons } from '@feathersjs/feathers';
-import { AuthenticationService, JWTStrategy } from '@feathersjs/authentication';
-import { LocalStrategy } from '@feathersjs/authentication-local';
-import { expressOauth } from '@feathersjs/authentication-oauth';
+import { ServiceAddons, Params } from "@feathersjs/feathers";
+import { AuthenticationService, JWTStrategy } from "@feathersjs/authentication";
+import { LocalStrategy } from "@feathersjs/authentication-local";
+import {
+  expressOauth,
+  OAuthStrategy,
+  OAuthProfile
+} from "@feathersjs/authentication-oauth";
 
-import { Application } from './declarations';
+import { Application } from "./declarations";
 
-declare module './declarations' {
+declare module "./declarations" {
   interface ServiceTypes {
-    'authentication': AuthenticationService & ServiceAddons<any>;
+    authentication: AuthenticationService & ServiceAddons<any>;
+  }
+}
+
+class GithubStrategy extends OAuthStrategy {
+  async getEntityData(profile: OAuthProfile, existing: any, params: Params) {
+    const baseData = await super.getEntityData(profile, existing, params);
+
+    return {
+      ...baseData,
+      email: profile.email
+    };
   }
 }
 
 export default function(app: Application) {
   const authentication = new AuthenticationService(app);
 
-  authentication.register('jwt', new JWTStrategy());
-  authentication.register('local', new LocalStrategy());
+  authentication.register("jwt", new JWTStrategy());
+  authentication.register("local", new LocalStrategy());
+  authentication.register("github", new GithubStrategy());
 
-  app.use('/authentication', authentication);
+  app.use("/authentication", authentication);
   app.configure(expressOauth());
 }
--- src/services/users/users.class.ts
+++ src/services/users/users.class.ts
@@ -1,10 +1,10 @@
-import { Service, NedbServiceOptions } from 'feathers-nedb';
-import { Application } from '../../declarations';
-import crypto from 'crypto';
-import { Params } from 'express-serve-static-core';
+import { Service, NedbServiceOptions } from "feathers-nedb";
+import { Application } from "../../declarations";
+import crypto from "crypto";
+import { Params } from "express-serve-static-core";
 
-const gravatarUrl = 'https://s.gravatar.com/avatar';
-const query = 's=60';
+const gravatarUrl = "https://s.gravatar.com/avatar";
+const query = "s=60";
 
 interface UserData {
   _id?: string;
@@ -19,9 +19,12 @@ export class Users extends Service {
     super(options);
   }
 
-  create (data: UserData, params?: Params) {
+  create(data: UserData, params?: Params) {
     const { email, password, githubId } = data;
-    const hash = crypto.createHash('md5').update(email.toLowerCase()).digest('hex');
+    const hash = crypto
+      .createHash("md5")
+      .update(email.toLowerCase())
+      .digest("hex");
     const avatar = `${gravatarUrl}/${hash}?${query}`;
     const userData = {
       email,
@@ -32,4 +35,4 @@ export class Users extends Service {
 
     return super.create(userData, params);
   }
-};
+}
